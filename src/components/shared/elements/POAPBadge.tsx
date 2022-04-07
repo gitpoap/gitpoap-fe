5ddaa35d6067d4190a3f81196f6ca72fc33693c1
Aldo Lamberti <jlamberti2015@gmail.com>
2022-04-07 19:11:46 -0400

    Fix POAP Height GitPOAP is of height 162, POAP has a height of 150 and margin of 6

--- src/components/shared/elements/POAPBadge.tsx
+++ src/components/shared/elements/POAPBadge.tsx
@@ -25,6 +25,8 @@ const ImgContainer = styled.a`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  // Account for hex height
+  margin: ${rem(6)} 0;
 `;
 
 const POAP = styled(Image)`
@@ -44,7 +46,7 @@ const POAP = styled(Image)`
 `;
 
 const Title = styled(TitleUI)`
-  margin-top: ${rem(18)};
+  margin-top: ${rem(10)};
   color: ${TextLight};
   width: ${rem(170)};
 `;
