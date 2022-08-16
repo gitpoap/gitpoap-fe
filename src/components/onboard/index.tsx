1c24a7762e0c08b1d3824fb9820ae1718c68407a
Aldo Lamberti <jlamberti2015@gmail.com>
2022-08-16 14:19:32 -0400

    Improve copy for entry page

--- src/components/onboard/index.tsx
+++ src/components/onboard/index.tsx
@@ -1,11 +1,17 @@
-import { Center, Container, Stack } from '@mantine/core';
+import { Center, Container, List, Stack } from '@mantine/core';
 import { rem } from 'polished';
 import { GoMarkGithub } from 'react-icons/go';
+import styled from 'styled-components';
+import { PrimaryBlue } from '../../colors';
 
 import { useAuthContext } from '../github/AuthContext';
-import { Button, Header, Text } from '../shared/elements';
+import { Button, Text } from '../shared/elements';
 import { StyledLink } from './Completed';
 import { IntakeForm } from './IntakeForm';
+
+const Bold = styled.b`
+  color: ${PrimaryBlue};
+`;
 
 export const OnboardingPage = () => {
   const { tokens, authorizeGitHub, isLoggedIntoGitHub, user } = useAuthContext();
@@ -15,11 +21,34 @@ export const OnboardingPage = () => {
       <Container>
         <Center>
           <Stack my="xl" spacing="xl">
-            <Header>{'GitPOAP Onboarding'}</Header>
+            <Text style={{ fontSize: rem(40), lineHeight: rem(40), textAlign: 'center' }}>
+              {'GitPOAP Onboarding'}
+            </Text>
+            <Text>{"Welcome! Here's a bit more info for you:"}</Text>
             <Text>
-              {
-                "We'll award 'Annual Contributor' GitPOAPs to anyone who has had at least one merged pull request from the founding year to the current year (e.g. '2020 ProjectName Contributor'). You can award GitPOAPs to individual repos, a group of repos, or your whole org - that's up to you."
-              }
+              <List
+                style={{ color: 'inherit', font: 'inherit', padding: `0 ${rem(24)}` }}
+                spacing="sm"
+              >
+                <List.Item>
+                  <Bold>Annual Contributor GitPOAPs:</Bold>
+                  {
+                    " We'll award a GitPOAP to anyone who has had a Pull Request merged to your project in each given year"
+                  }
+                </List.Item>
+                <List.Item>
+                  <Bold>Customization:</Bold>
+                  {
+                    ' You can select which repos are eligible, and can award a separate GitPOAP per repo or one across a group of repos'
+                  }
+                </List.Item>
+                <List.Item>
+                  <Bold>Historical and Ongoing:</Bold>
+                  {
+                    " We'll comb through the contribution history on GitHub and award GitPOAPs to everyone who has contributed since the start, as well as everyone going forward"
+                  }
+                </List.Item>
+              </List>
             </Text>
             <Text>
               {
