664305a5b1173996813ae201be0b37026597b5e7
Aldo Lamberti <jlamberti2015@gmail.com>
2022-09-20 17:05:47 -0400

    add text to Text test

--- __tests__/components/shared/elements/Text.test.tsx
+++ __tests__/components/shared/elements/Text.test.tsx
@@ -4,7 +4,7 @@ import { Text } from '../../../../src/components/shared/elements';
 
 describe('Text', () => {
   it('renders a Text', () => {
-    const { container } = render(<Text />);
+    const { container } = render(<Text>{'Text'}</Text>);
     const text = container.firstChild;
 
     expect(text).toBeInTheDocument();
