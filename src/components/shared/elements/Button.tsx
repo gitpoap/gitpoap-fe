bef7fcf8bf9bd8c504bcbd56ae4006e029650a5b
Aldo Lamberti <jlamberti2015@gmail.com>
2022-09-21 15:43:08 -0400

    Simplify button export

--- src/components/shared/elements/Button.tsx
+++ src/components/shared/elements/Button.tsx
@@ -1,6 +1,1 @@
-import React from 'react';
-import { Button as ButtonUI, ButtonProps } from '@mantine/core';
-
-export const Button = (props: ButtonProps) => {
-  return <ButtonUI {...props}>{props.children}</ButtonUI>;
-};
+export { Button } from '@mantine/core';
