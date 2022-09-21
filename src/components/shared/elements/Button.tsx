230e6c8acb73fb62b0aff5edc28fd027a20f4bf3
Aldo Lamberti <jlamberti2015@gmail.com>
2022-09-21 16:12:58 -0400

    undo simplifaction, need to explicitly state props

--- src/components/shared/elements/Button.tsx
+++ src/components/shared/elements/Button.tsx
@@ -1,1 +1,18 @@
-export { Button } from '@mantine/core';
+import { Button as ButtonUI, ButtonProps } from '@mantine/core';
+import React from 'react';
+
+type Props = ButtonProps & {
+  className?: string;
+  onClick?: React.MouseEventHandler;
+  onMouseEnter?: React.MouseEventHandler;
+  onMouseLeave?: React.MouseEventHandler;
+  id?: string;
+};
+
+export const Button = (props: Props) => {
+  return (
+    <ButtonUI {...props} onClick={() => {}}>
+      {props.children}
+    </ButtonUI>
+  );
+};
