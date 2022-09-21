cf674e5385fddafedf19f46baf6c2dba613d7892
Aldo Lamberti <jlamberti2015@gmail.com>
2022-09-21 15:34:26 -0400

    Change styledcomponents overrides to theme object override

---
+++ src/lib/theme/ButtonTheme.ts
@@ -0,0 +1,61 @@
+import { ButtonStylesParams, MantineTheme } from '@mantine/core';
+import { DarkGray, ExtraHover, ExtraPressed, PrimaryBlue, TextGray, White } from '../../colors';
+import { rem } from 'polished';
+
+export const ButtonTheme = {
+  defaultProps: {
+    radius: 6,
+    uppercase: true,
+  },
+  styles: (theme: MantineTheme, params: ButtonStylesParams) => ({
+    root: {
+      letterSpacing: rem(2),
+      transition: '150ms background ease, 150ms color ease, 150ms border ease',
+      ...(params.variant === 'filled' && ButtonFilled),
+      ...(params.variant === 'outline' && ButtonOutline),
+    },
+  }),
+};
+
+const ButtonFilled = {
+  background: PrimaryBlue,
+  '&:disabled': {
+    backgroundColor: DarkGray,
+    '.mantine-Button-label': {
+      color: TextGray,
+    },
+  },
+  '&:active:not(:disabled)': {
+    backgroundColor: ExtraPressed,
+  },
+  '&:hover:not(:disabled)': {
+    backgroundColor: ExtraHover,
+  },
+  '&[data-loading]': {
+    backgroundColor: PrimaryBlue,
+    '.mantine-Button-label': {
+      color: White,
+    },
+  },
+};
+
+const ButtonOutline = {
+  background: 'transparent',
+  borderColor: TextGray,
+  color: White,
+  '&:disabled': {
+    backgroundColor: 'transparent',
+    borderColor: TextGray,
+    '.mantine-Button-label': {
+      color: TextGray,
+    },
+  },
+  '&:active:not(:disabled)': {
+    borderColor: ExtraPressed,
+    color: ExtraPressed,
+  },
+  '&:hover:not(:disabled)': {
+    borderColor: ExtraHover,
+    color: ExtraHover,
+  },
+};
