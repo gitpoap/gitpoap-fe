62b60600d8cae6fe4613078411bfe6b2e4bb96d3
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-10-19 19:46:35 -0400

    feat: create check eligibility page

--- src/pages/eligibility.tsx
+++ src/pages/eligibility.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { Grid } from '@mantine/core';
-import { Page } from '../_app';
-import { CheckEligibility } from '../../components/check/CheckEligibility';
+import { Page } from './_app';
+import { CheckEligibility } from '../components/check/CheckEligibility';
 
-const Search: Page = () => {
+const CheckEligibilityPage: Page = () => {
   const router = useRouter();
-  const searchQuery = router.query.query as string;
+  const searchQuery = router.query.search as string | undefined;
 
   return (
     <>
       <Head>
-        <title>{`${searchQuery ?? 'Check Eligibility '} | GitPOAP`}</title>
+        <title>{`${
+          searchQuery ? `${searchQuery} - Check Eligibility` : 'Check Eligibility'
+        } | GitPOAP`}</title>
         <meta
           name="GitPOAP is a decentralized reputation platform that represents off-chain accomplishments and contributions on chain as POAPs."
           content="GitPOAP Eligibility Check"
@@ -27,4 +29,4 @@ const Search: Page = () => {
   );
 };
 
-export default Search;
+export default CheckEligibilityPage;
