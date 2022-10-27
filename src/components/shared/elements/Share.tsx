b30964d14ce027365c07de57dcf69f5af36098c5
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-10-27 13:41:55 -0400

    refactor: use more mantine on InfoHexProfileDetail

--- src/components/shared/elements/Share.tsx
+++ src/components/shared/elements/Share.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import { rem } from 'polished';
 import { useClipboard } from '@mantine/hooks';
-import { Box, Group, Tooltip } from '@mantine/core';
+import { Box, Group, GroupProps, Tooltip } from '@mantine/core';
 import { FiShare } from 'react-icons/fi';
 import { LinkStyles } from '../../../components/shared/elements/NavLink';
 
 type Props = {
   className?: string;
   textToCopy: string;
-};
+} & GroupProps;
 
 const ShareText = styled(Box)`
   ${LinkStyles}
@@ -20,7 +20,7 @@ const Icon = styled(FiShare)`
   margin-left: ${rem(5)};
 `;
 
-export const Share = ({ className, textToCopy }: Props) => {
+export const Share = ({ className, textToCopy, ...groupProps }: Props) => {
   const clipboard = useClipboard({ timeout: 1500 });
 
   return (
@@ -31,6 +31,7 @@ export const Share = ({ className, textToCopy }: Props) => {
       align="center"
       spacing={0}
       sx={{ display: 'inline-flex' }}
+      {...groupProps}
     >
       <Tooltip opened={clipboard.copied} label="Copied!" withArrow position="right">
         <ShareText>
