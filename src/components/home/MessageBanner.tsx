e9d83d287c447ca324ffb4ababb35f153f03a9e4
tyler415git <tyler@gitpoap.io>
2022-10-27 10:30:09 -0700

    feat: use Mantine alert for message banner

--- src/components/home/MessageBanner.tsx
+++ src/components/home/MessageBanner.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import Link from 'next/link';
 import { rem } from 'polished';
 import styled from 'styled-components';
-import { Group, Stack, Text, TextProps } from '@mantine/core';
+import { Group, Text, TextProps, Alert } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 import { BackgroundPanel2, TextGray, TextLight } from '../../colors';
 import { BREAKPOINTS } from '../../constants';
 
-const MessageBannerContent = styled(Group)`
+const MessageBannerContent = styled(Alert)`
   background-color: ${BackgroundPanel2};
-  border-radius: ${rem(10)};
-  cursor: pointer;
 `;
 
 const Title = styled(Text)<TextProps>`
@@ -39,15 +37,11 @@ export const MessageBanner = ({
   return (
     <Group position="center" my={rem(20)}>
       <Link href={href}>
-        <MessageBannerContent spacing="md" align="center" py={rem(20)} px={rem(15)} noWrap>
-          {!matchesBreakpointSmall && leftIcon ? leftIcon : ''}
-
-          <Stack justify="center" align="start" spacing="xs">
-            <Title size={20}>{title}</Title>
+        <MessageBannerContent icon={leftIcon} title={<Title size={20}>{title}</Title>} radius="md">
+          <Group align="center" spacing="sm">
             <Message size={16}>{message}</Message>
-          </Stack>
-
-          {!matchesBreakpointSmall && rightIcon ? rightIcon : ''}
+            {!matchesBreakpointSmall && rightIcon ? rightIcon : ''}
+          </Group>
         </MessageBannerContent>
       </Link>
     </Group>
