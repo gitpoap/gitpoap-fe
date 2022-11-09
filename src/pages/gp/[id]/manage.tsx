e1e039c16926ad76c44f28ba31b10571bb1190c9
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2022-11-09 13:07:45 -0500

    feat: only allow admins to see the page for now

--- src/pages/gp/[id]/manage.tsx
+++ src/pages/gp/[id]/manage.tsx
@@ -35,7 +35,11 @@ const ManageGitPOAPPage: NextPage = () => {
       </Head>
       <Grid justify="center" mt={rem(20)} mb={rem(20)} style={{ zIndex: 1, flex: 1 }}>
         <Grid.Col xs={10} sm={10} md={10} lg={10} xl={10}>
-          {address ? <ManageGitPOAP gitPOAPId={gitPOAPId} /> : <Login />}
+          {address && user.permissions.isAdmin ? (
+            <ManageGitPOAP gitPOAPId={gitPOAPId} />
+          ) : (
+            <Login />
+          )}
         </Grid.Col>
       </Grid>
     </>
