8bc9642e86aa9ecb0dcf1544592d103b539230bb
tyler415git <tyler@gitpoap.io>
2022-11-10 12:11:34 -0800

    fix: MessageBaner stories

--- src/stories/elements/MessageBanner.stories.tsx
+++ src/stories/elements/MessageBanner.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { FaEdge, FaArrowRight } from 'react-icons/fa';
+import { FaEdge } from 'react-icons/fa';
 import { MessageBanner } from '../../components/home/MessageBanner';
 
 export default {
@@ -19,5 +19,4 @@ Default.args = {
     'If you are going to Devcon, register at swag.gitpoap.io to get your personalized GitPOAP swag!',
   href: '/',
   leftIcon: <FaEdge />,
-  rightIcon: <FaArrowRight />,
 };
