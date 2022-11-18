a9d65206aa1f0440f520324cc367bee15a02b195
Aldo Lamberti <jlamberti2015@gmail.com>
2022-11-18 14:55:33 -0800

    Componetize CG Form (#222)

--- src/components/create/CreationForm.tsx
+++ src/components/create/CreationForm.tsx
@@ -1,21 +1,9 @@
-import {
-  Container,
-  Group,
-  Stack,
-  Input as InputUI,
-  Box,
-  Text,
-  List,
-  Grid,
-  Divider,
-} from '@mantine/core';
-import { rem } from 'polished';
+import { Container, Group } from '@mantine/core';
 import { useState } from 'react';
-import styled from 'styled-components';
 
-import { DateInput, Header, Input, TextArea, TextInputLabelStyles } from '../shared/elements';
+import { ButtonStatus } from '../shared/compounds/StatusButton';
+import { Header } from '../shared/elements';
 import { useCreationForm } from './useCreationForm';
-import { SelectContributors } from './SelectContributors';
 import { useApi } from '../../hooks/useApi';
 import {
   ContributorsObject,
@@ -23,16 +11,9 @@ import {
   ValidatedCreateFormValues,
   ValidatedContributor,
 } from '../../lib/api/gitpoapRequest';
-import { HexagonDropzone } from './HexagonDropzone';
-import { useRouter } from 'next/router';
-import { Link } from '../shared/compounds/Link';
-import { ExtraRed } from '../../colors';
 import { FileWithPath } from '@mantine/dropzone';
-import { ButtonStatus, StatusButton } from '../shared/compounds/StatusButton';
-
-const Label = styled(InputUI.Label)`
-  ${TextInputLabelStyles};
-`;
+import { FormFields } from './FormFields';
+import { useRouter } from 'next/router';
 
 const HeaderText = {
   UNSUBMITTED: 'Create GitPOAP',
@@ -41,37 +22,20 @@ const HeaderText = {
   REJECTED: 'Edit GitPOAP',
 };
 
-const SubmitButtonText = {
-  UNSUBMITTED: 'Create & Submit For Review',
-  APPROVED: 'Save & Submit Contributors',
-  PENDING: 'Save & Submit Changes',
-  REJECTED: 'Save & Submit For Rereview',
-};
-
 type AdminApprovalStatus = 'UNSUBMITTED' | 'APPROVED' | 'REJECTED' | 'PENDING';
 
 export const CreationForm = () => {
   const api = useApi();
-  const {
-    errors,
-    values,
-    getInputProps,
-    insertListItem,
-    isDirty,
-    removeListItem,
-    setFieldError,
-    setFieldValue,
-    validate,
-  } = useCreationForm();
+  const form = useCreationForm();
   const router = useRouter();
   const [buttonStatus, setButtonStatus] = useState<ButtonStatus>(ButtonStatus.INITIAL);
   const approvalStatus: AdminApprovalStatus = 'UNSUBMITTED';
-  const imageUrl = values.image ? URL.createObjectURL(values.image) : null;
+  const imageUrl = form.values.image ? URL.createObjectURL(form.values.image) : null;
 
   const submitCreateCustomGitPOAP = async (formValues: CreateFormValues) => {
     setButtonStatus(ButtonStatus.LOADING);
 
-    if (validate().hasErrors) {
+    if (form.validate().hasErrors) {
       setButtonStatus(ButtonStatus.ERROR);
       return;
     }
@@ -111,117 +75,20 @@ export const CreationForm = () => {
         <Header>{HeaderText[approvalStatus]}</Header>
         <Header>{approvalStatus}</Header>
       </Group>
-      <Stack align="center" spacing={32}>
-        <HexagonDropzone
-          imageUrl={imageUrl}
-          setError={setFieldError}
-          addImage={(image: FileWithPath) => setFieldValue('image', image)}
-          removeImage={() => {
-            setFieldValue('image', null);
-          }}
-        />
-        {Object.keys(errors).find((error) => /^image/.test(error)) && (
-          <Text style={{ color: ExtraRed }} inline>
-            {Object.keys(errors)
-              .filter((error) => /^image/.test(error))
-              .map((key) => errors[key])}
-          </Text>
-        )}
-        <Stack spacing={32} sx={{ maxWidth: '100%' }}>
-          <Box sx={{ maxWidth: '100%', width: rem(400) }}>
-            <Text>{'Image Requirements:'}</Text>
-            <List>
-              <List.Item>
-                <Group spacing={6}>
-                  <Text>{'Mandatory: PNG or GIF format,'}</Text>
-                  <Link href="/links/canva-template" target="_blank" rel="noopener noreferrer">
-                    <Text variant="link">{'GitPOAP Template'}</Text>
-                  </Link>
-                </Group>
-              </List.Item>
-              <List.Item>
-                <Text>{'Recommended: measures 500x500px, size less than 200KB (Max. 4MB)'}</Text>
-              </List.Item>
-              <List.Item>
-                <Link href="/links/design-guide" target="_blank" rel="noopener noreferrer">
-                  <Text variant="link">{'Design Guide'}</Text>
-                </Link>
-              </List.Item>
-            </List>
-          </Box>
-          <Input
-            required
-            style={{ width: '100%' }}
-            label="GitPOAP Name"
-            placeholder="Contributor 2022"
-            {...getInputProps('name')}
-          />
-          <TextArea
-            required
-            style={{ width: '100%' }}
-            label="Description"
-            placeholder="For all our valuable contributors in 2022"
-            {...getInputProps('description')}
-          />
-          <Box>
-            <Label mb={rem(11)} required>
-              {'Accomplishment Period'}
-            </Label>
-            <Grid>
-              <Grid.Col xs={6} span={12}>
-                <DateInput
-                  maxDate={values.endDate}
-                  placeholder="Start Date"
-                  weekendDays={[]}
-                  sx={{ width: '100%', minWidth: rem(220) }}
-                  {...getInputProps('startDate')}
-                />
-              </Grid.Col>
-              <Grid.Col xs={6} span={12}>
-                <DateInput
-                  minDate={values.startDate}
-                  placeholder="End Date"
-                  weekendDays={[]}
-                  sx={{ width: '100%', minWidth: rem(220) }}
-                  {...getInputProps('endDate')}
-                />
-              </Grid.Col>
-            </Grid>
-          </Box>
-          <Input
-            required
-            style={{ width: '100%' }}
-            label="Email"
-            placeholder="Email"
-            disabled={false}
-            {...getInputProps('creatorEmail')}
-          />
-        </Stack>
-        <Box my={32}>
-          <Divider
-            mb={32}
-            labelPosition="center"
-            label={<Header>{'Recipients'}</Header>}
-            variant="dashed"
-          />
-          <SelectContributors
-            contributors={values.contributors}
-            addContributor={(item) => insertListItem('contributors', item)}
-            removeContributor={(index) => removeListItem('contributors', index)}
-          />
-        </Box>
-        <StatusButton
-          onClick={async () => await submitCreateCustomGitPOAP(values)}
-          isDisabled={
-            !isDirty() ||
-            buttonStatus === ButtonStatus.SUCCESS ||
-            buttonStatus === ButtonStatus.LOADING
-          }
-          status={buttonStatus}
-        >
-          {SubmitButtonText[approvalStatus]}
-        </StatusButton>
-      </Stack>
+      <FormFields
+        approvalStatus={'UNSUBMITTED'}
+        buttonStatus={buttonStatus}
+        imageUrl={imageUrl}
+        isDisabled={
+          !form.isDirty() ||
+          buttonStatus === ButtonStatus.SUCCESS ||
+          buttonStatus === ButtonStatus.LOADING
+        }
+        form={form}
+        addImage={(image: FileWithPath) => form.setFieldValue('image', image)}
+        handleSubmit={submitCreateCustomGitPOAP}
+        removeImage={() => form.setFieldValue('image', null)}
+      />
     </Container>
   );
 };
--- src/components/create/EditForm.tsx
+++ src/components/create/EditForm.tsx
@@ -1,25 +1,10 @@
-import {
-  Container,
-  Group,
-  Stack,
-  Input as InputUI,
-  Box,
-  Text,
-  List,
-  Grid,
-  Divider,
-} from '@mantine/core';
-import { rem } from 'polished';
+import { Container, Group } from '@mantine/core';
 import { useState } from 'react';
-import styled from 'styled-components';
 
-import { DateInput, Header, Input, TextArea, TextInputLabelStyles } from '../shared/elements';
-import { SelectContributors } from './SelectContributors';
+import { ButtonStatus } from '../shared/compounds/StatusButton';
+import { Header } from '../shared/elements';
 import { useApi } from '../../hooks/useApi';
-import { HexagonDropzone } from './HexagonDropzone';
 import { useRouter } from 'next/router';
-import { Link } from '../shared/compounds/Link';
-import { ExtraRed } from '../../colors';
 import { useEditForm } from './useEditForm';
 import { FileWithPath } from '@mantine/dropzone';
 import {
@@ -28,24 +13,13 @@ import {
   ValidatedContributor,
   ValidatedEditFormValues,
 } from '../../lib/api/gitpoapRequest';
-import { ButtonStatus, StatusButton } from '../shared/compounds/StatusButton';
-
-const Label = styled(InputUI.Label)`
-  ${TextInputLabelStyles};
-`;
+import { FormFields } from './FormFields';
 
 const HeaderText = {
   UNSUBMITTED: 'Create GitPOAP',
   APPROVED: 'Add Contributors',
   PENDING: 'Edit GitPOAP',
   REJECTED: 'Edit GitPOAP',
-};
-
-const SubmitButtonText = {
-  UNSUBMITTED: 'Create & Submit For Review',
-  APPROVED: 'Save & Submit Contributors',
-  PENDING: 'Save & Submit Changes',
-  REJECTED: 'Save & Submit For Re-review',
 };
 
 type AdminApprovalStatus = 'UNSUBMITTED' | 'APPROVED' | 'REJECTED' | 'PENDING';
@@ -67,30 +41,20 @@ export const EditForm = ({
 }: Props) => {
   const api = useApi();
   const [hasRemovedSavedImage, setHasRemovedSavedImage] = useState(false);
-  const {
-    errors,
-    values,
-    getInputProps,
-    insertListItem,
-    isDirty,
-    removeListItem,
-    setFieldError,
-    setFieldValue,
-    validate,
-  } = useEditForm(initialValues, hasRemovedSavedImage);
+  const form = useEditForm(initialValues, hasRemovedSavedImage);
   const router = useRouter();
   const [buttonStatus, setButtonStatus] = useState<ButtonStatus>(ButtonStatus.INITIAL);
 
   const imageUrl = hasRemovedSavedImage
-    ? values.image
-      ? URL.createObjectURL(values.image)
+    ? form.values.image
+      ? URL.createObjectURL(form.values.image)
       : null
     : savedImageUrl;
 
   const submitEditCustomGitPOAP = async (formValues: EditFormValues) => {
     setButtonStatus(ButtonStatus.LOADING);
 
-    if (validate().hasErrors) {
+    if (form.validate().hasErrors) {
       setButtonStatus(ButtonStatus.ERROR);
       return;
     }
@@ -130,135 +94,34 @@ export const EditForm = ({
         <Header>{HeaderText[adminApprovalStatus]}</Header>
         <Header>{adminApprovalStatus}</Header>
       </Group>
-      <Stack align="center" spacing={32}>
-        <HexagonDropzone
-          imageUrl={imageUrl}
-          setError={setFieldError}
-          addImage={(image: FileWithPath) => {
-            if (!hasRemovedSavedImage) {
-              setHasRemovedSavedImage(true);
-            }
-            setFieldValue('image', image);
-          }}
-          removeImage={() => {
-            if (hasRemovedSavedImage) {
-              setFieldValue('image', null);
-            } else {
-              setHasRemovedSavedImage(true);
-            }
-          }}
-        />
-        {Object.keys(errors).find((error) => /^image/.test(error)) && (
-          <Text style={{ color: ExtraRed }} inline>
-            {Object.keys(errors)
-              .filter((error) => /^image/.test(error))
-              .map((key) => errors[key])}
-          </Text>
-        )}
-        <Stack spacing={32} sx={{ maxWidth: '100%' }}>
-          <Box sx={{ maxWidth: '100%', width: rem(400) }}>
-            <Text>{'Image Requirements:'}</Text>
-            <List>
-              <List.Item>
-                <Group spacing={6}>
-                  <Text>{'Mandatory: PNG or GIF format,'}</Text>
-                  <Link
-                    href="https://www.canva.com/design/DAFQoFm0dhQ/H17FASlR17kwLk6m303hBw/view?utm_content=DAFQoFm0dhQ&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink&mode=preview"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Text variant="link">{'GitPOAP Template'}</Text>
-                  </Link>
-                </Group>
-              </List.Item>
-              <List.Item>
-                <Text>{'Recommended: measures 500x500px, size less than 200KB (Max. 4MB)'}</Text>
-              </List.Item>
-              <List.Item>
-                <Link
-                  href="https://gitpoap.notion.site/GitPOAP-Design-Guide-Requirements-9a843acfe1c7490bbfcdab2d1a47e8af"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Text variant="link">{'Design Guide'}</Text>
-                </Link>
-              </List.Item>
-            </List>
-          </Box>
-          <Input
-            required
-            style={{ width: '100%' }}
-            label="GitPOAP Name"
-            placeholder="Contributor 2022"
-            {...getInputProps('name')}
-          />
-          <TextArea
-            required
-            style={{ width: '100%' }}
-            label="Description"
-            placeholder="For all our valuable contributors in 2022"
-            {...getInputProps('description')}
-          />
-          <Box>
-            <Label mb={rem(11)} required>
-              {'Accomplishment Period'}
-            </Label>
-            <Grid>
-              <Grid.Col xs={6} span={12}>
-                <DateInput
-                  maxDate={values.endDate}
-                  placeholder="Start Date"
-                  weekendDays={[]}
-                  sx={{ width: '100%', minWidth: rem(220) }}
-                  {...getInputProps('startDate')}
-                />
-              </Grid.Col>
-              <Grid.Col xs={6} span={12}>
-                <DateInput
-                  minDate={values.startDate}
-                  placeholder="End Date"
-                  weekendDays={[]}
-                  sx={{ width: '100%', minWidth: rem(220) }}
-                  {...getInputProps('endDate')}
-                />
-              </Grid.Col>
-            </Grid>
-          </Box>
-          <Input
-            required
-            style={{ width: '100%' }}
-            label="Email"
-            placeholder="Email"
-            value={creatorEmail}
-            disabled={true}
-          />
-        </Stack>
-        <Box my={32}>
-          <Divider
-            mb={32}
-            labelPosition="center"
-            label={<Header>{'Recipients'}</Header>}
-            variant="dashed"
-          />
-          <SelectContributors
-            contributors={values.contributors}
-            addContributor={(item) => insertListItem('contributors', item)}
-            removeContributor={(index) => removeListItem('contributors', index)}
-          />
-        </Box>
-        <StatusButton
-          onClick={async () => await submitEditCustomGitPOAP(values)}
-          isDisabled={
-            (!isDirty() &&
-              JSON.stringify(initialValues.contributors) === JSON.stringify(values.contributors)) ||
-            buttonStatus === ButtonStatus.SUCCESS ||
-            buttonStatus === ButtonStatus.LOADING
+      <FormFields
+        approvalStatus={adminApprovalStatus}
+        buttonStatus={buttonStatus}
+        creatorEmail={creatorEmail}
+        imageUrl={imageUrl}
+        isDisabled={
+          (!form.isDirty() &&
+            JSON.stringify(initialValues.contributors) ===
+              JSON.stringify(form.values.contributors)) ||
+          buttonStatus === ButtonStatus.SUCCESS ||
+          buttonStatus === ButtonStatus.LOADING
+        }
+        form={form}
+        addImage={(image: FileWithPath) => {
+          if (!hasRemovedSavedImage) {
+            setHasRemovedSavedImage(true);
           }
-          status={buttonStatus}
-        >
-          {SubmitButtonText[adminApprovalStatus]}
-        </StatusButton>
-      </Stack>
+          form.setFieldValue('image', image);
+        }}
+        handleSubmit={submitEditCustomGitPOAP}
+        removeImage={() => {
+          if (hasRemovedSavedImage) {
+            form.setFieldValue('image', null);
+          } else {
+            setHasRemovedSavedImage(true);
+          }
+        }}
+      />
     </Container>
   );
 };
