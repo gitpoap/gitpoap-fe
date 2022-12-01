857672430d96c3ecadea209cf7dae04d39e611ae
tyler415git <tyler@gitpoap.io>
2022-12-01 06:52:38 -0800

    feat: store signature by address into IndexedDB

--- src/hooks/useConnectionStatus.ts
+++ src/hooks/useConnectionStatus.ts
@@ -1,7 +1,4 @@
-import { useState, useEffect } from 'react';
-import { useWeb3React } from '@web3-react/core';
-import { useTokens } from './useTokens';
-import { useIndexedDB } from './useIndexedDB';
+import { useState } from 'react';
 
 export enum ConnectionStatus {
   CONNECTED_TO_WALLET = 'connected-to-wallet' /* Connected to wallet & authenticated */,
@@ -17,31 +14,9 @@ export enum ConnectionStatus {
  * @returns connection status getter and setter.
  */
 export const useConnectionStatus = () => {
-  const { account, library } = useWeb3React();
-  const isConnected = typeof account === 'string' && !!library;
-  const { tokens } = useTokens();
-
-  const { value: signature } = useIndexedDB('signature');
-  const { value: signedAddress } = useIndexedDB('address');
-
   const [connectionStatus, setConnectionStatus] = useState<ConnectionStatus>(
     ConnectionStatus.UNINITIALIZED,
   );
-
-  useEffect(() => {
-    // we check if wallet is connected, access_token/refresh_token exist, and signature is signed by connected address
-    if (
-      isConnected &&
-      signature &&
-      signedAddress === account &&
-      tokens?.accessToken &&
-      tokens?.refreshToken
-    ) {
-      setConnectionStatus(ConnectionStatus.CONNECTED_TO_WALLET);
-    } else {
-      setConnectionStatus(ConnectionStatus.DISCONNECTED);
-    }
-  }, [isConnected, tokens, signature, signedAddress, account]);
 
   return { connectionStatus, setConnectionStatus };
 };
