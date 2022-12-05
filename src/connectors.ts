86a667759137c7d2025e5c8efbc5552290e04835
tyler415git <tyler@gitpoap.io>
2022-12-05 05:12:56 -0800

    feat: move connectors

--- src/connectors.ts
+++ src/connectors.ts
@@ -2,22 +2,20 @@ import { WalletLinkConnector } from '@web3-react/walletlink-connector';
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
 import { InjectedConnector } from '@web3-react/injected-connector';
 
-const supportedChainIds = [1, 3, 4, 5, 42];
-
 const walletlink = new WalletLinkConnector({
   url: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
   appName: 'gitpoap-fe',
-  supportedChainIds,
+  supportedChainIds: [1, 3, 4, 5, 42],
 });
 
 const walletconnect = new WalletConnectConnector({
-  rpc: { 1: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}` },
+  rpc: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
 });
 
 const injected = new InjectedConnector({
-  supportedChainIds,
+  supportedChainIds: [1, 3, 4, 5, 42],
 });
 
 export const connectors = {
