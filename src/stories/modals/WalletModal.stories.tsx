a4641a5f882c3733280fcc6e360da4fa6102bb10
tyler415git <tyler@gitpoap.io>
2022-12-08 04:00:26 -0800

    feat: update wallet modal story

--- src/stories/modals/WalletModal.stories.tsx
+++ src/stories/modals/WalletModal.stories.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import WalletModal from '../../components/wallet/WalletModal';
-import { getWeb3Provider } from '../../helpers';
 import { Web3ReactProvider } from '@web3-react/core';
 import { Web3ContextProvider } from '../../components/wallet/Web3Context';
+import { connectors } from '../../connectors';
 
 export default {
   title: 'Modals/WalletModal',
@@ -12,7 +12,7 @@ export default {
 
 const Template: ComponentStory<typeof WalletModal> = (args) => {
   return (
-    <Web3ReactProvider getLibrary={getWeb3Provider}>
+    <Web3ReactProvider connectors={connectors}>
       <Web3ContextProvider>
         <WalletModal {...args} />
       </Web3ContextProvider>
