b349c9cf0f07b9517af88a1e86929631ede9dc89
tyler415git <tyler@gitpoap.io>
2022-12-22 17:44:31 -0800

    fix: memberships story

--- src/stories/pages/me/memberships.stories.tsx
+++ src/stories/pages/me/memberships.stories.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { createClient, Provider as URQLProvider } from 'urql';
 import UserMemberships from '../../../pages/me/memberships';
 import { Layout } from '../../../components/Layout';
+import { urqlClientOptions } from '../../../lib/urql';
 
 export default {
   title: 'Pages/Me/Memberships',
   component: UserMemberships,
 } as ComponentMeta<typeof UserMemberships>;
 
+const client = createClient(urqlClientOptions);
+
 const Template: ComponentStory<typeof UserMemberships> = () => {
   return (
-    <Layout>
-      <UserMemberships />
-    </Layout>
+    <URQLProvider value={client}>
+      <Layout>
+        <UserMemberships />
+      </Layout>
+    </URQLProvider>
   );
 };
 
