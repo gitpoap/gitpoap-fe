8b4e0c2a6466cfe309c849a8f0aa411298d962fb
Aldo Lamberti <jlamberti2015@gmail.com>
2023-01-13 15:35:31 -0500

    Create Select theme overrides Add custom select component to FormFields of the CG creation form

--- src/components/create/FormFields.tsx
+++ src/components/create/FormFields.tsx
@@ -1,4 +1,14 @@
-import { Group, Stack, Input as InputUI, Box, Text, List, Grid, Divider } from '@mantine/core';
+import {
+  Group,
+  Stack,
+  Input as InputUI,
+  Box,
+  Text,
+  List,
+  Grid,
+  Divider,
+  Select,
+} from '@mantine/core';
 import { FileWithPath } from '@mantine/dropzone';
 import { UseFormReturnType } from '@mantine/form';
 import { rem } from 'polished';
@@ -11,6 +21,9 @@ import { CreateFormValues, EditFormValues } from '../../lib/api/gitpoapRequest';
 import { HexagonDropzone } from './HexagonDropzone';
 import { Link } from '../shared/compounds/Link';
 import { ExtraRed } from '../../colors';
+import { useTeamsContext } from '../team/TeamsContext';
+import { forwardRef } from 'react';
+import { TeamLogo } from '../team/settings/TeamLogo';
 
 const Label = styled(InputUI.Label)`
   ${TextInputLabelStyles};
@@ -29,6 +42,7 @@ type Props<FormValues> = {
   approvalStatus: StaffApprovalStatus;
   buttonStatus: ButtonStatus;
   creatorEmail?: string;
+  teamName?: string;
   imageUrl: string | null;
   isDisabled: boolean;
   form: UseFormReturnType<FormValues>;
@@ -37,131 +51,182 @@ type Props<FormValues> = {
   removeImage: () => void;
 };
 
+interface ItemProps extends React.ComponentPropsWithoutRef<'div'> {
+  color: string;
+  imageUrl: string;
+  label: string;
+}
+
+const SelectItem = forwardRef<HTMLDivElement, ItemProps>(
+  ({ color, imageUrl, label, ...props }: ItemProps, ref) => (
+    <div ref={ref} {...props}>
+      <Group noWrap>
+        <TeamLogo color={color} imageUrl={imageUrl} name={label} size={32} />
+
+        <div>
+          <Text size="sm">{label}</Text>
+        </div>
+      </Group>
+    </div>
+  ),
+);
+
 export const FormFields = <FormValues extends CreateFormValues | EditFormValues>({
   approvalStatus,
   buttonStatus,
   creatorEmail,
+  teamName,
   imageUrl,
   isDisabled,
   form,
   addImage,
   handleSubmit,
   removeImage,
-}: Props<FormValues>) => (
-  <Stack align="center" spacing={32}>
-    <HexagonDropzone
-      imageUrl={imageUrl}
-      setError={form.setFieldError}
-      addImage={addImage}
-      removeImage={removeImage}
-    />
-    {Object.keys(form.errors).find((error) => /^image/.test(error)) && (
-      <Text style={{ color: ExtraRed }} inline>
-        {Object.keys(form.errors)
-          .filter((error) => /^image/.test(error))
-          .map((key) => form.errors[key])}
-      </Text>
-    )}
-    <Stack spacing={32} sx={{ maxWidth: '100%' }}>
-      <Box sx={{ maxWidth: '100%', width: rem(400) }}>
-        <Text>{'Image Requirements:'}</Text>
-        <List>
-          <List.Item>
-            <Group spacing={6}>
-              <Text>{'Mandatory: PNG or GIF format,'}</Text>
-              <Link href="/links/canva-template" target="_blank" rel="noopener noreferrer">
-                <Text variant="link">{'GitPOAP Template'}</Text>
-              </Link>
-            </Group>
-          </List.Item>
-          <List.Item>
-            <Text>{'Recommended: measures 500x500px, size less than 200KB (Max. 4MB)'}</Text>
-          </List.Item>
-          <List.Item>
-            <Link href="/links/design-guide" target="_blank" rel="noopener noreferrer">
-              <Text variant="link">{'Design Guide'}</Text>
-            </Link>
-          </List.Item>
-        </List>
-      </Box>
-      <Input
-        required
-        style={{ width: '100%' }}
-        label="GitPOAP Name"
-        placeholder="Contributor 2022"
-        {...form.getInputProps('name')}
+}: Props<FormValues>) => {
+  const teams = useTeamsContext();
+
+  return (
+    <Stack align="center" spacing={32}>
+      <HexagonDropzone
+        imageUrl={imageUrl}
+        setError={form.setFieldError}
+        addImage={addImage}
+        removeImage={removeImage}
       />
-      <TextArea
-        required
-        style={{ width: '100%' }}
-        label="Description"
-        placeholder="For all our valuable contributors in 2022"
-        {...form.getInputProps('description')}
-      />
-      <Box>
-        <Label mb={rem(11)} required>
-          {'Accomplishment Period'}
-        </Label>
-        <Grid>
-          <Grid.Col xs={6} span={12}>
-            <DateInput
-              maxDate={form.values.endDate}
-              placeholder="Start Date"
-              weekendDays={[]}
-              sx={{ width: '100%', minWidth: rem(220) }}
-              {...form.getInputProps('startDate')}
-            />
-          </Grid.Col>
-          <Grid.Col xs={6} span={12}>
-            <DateInput
-              minDate={form.values.startDate}
-              placeholder="End Date"
-              weekendDays={[]}
-              sx={{ width: '100%', minWidth: rem(220) }}
-              {...form.getInputProps('endDate')}
-            />
-          </Grid.Col>
-        </Grid>
-      </Box>
-      {creatorEmail ? (
-        <Input
-          required
-          style={{ width: '100%' }}
-          label="Email"
-          placeholder="Email"
-          value={creatorEmail}
-          disabled={true}
-        />
-      ) : (
-        <Input
-          required
-          style={{ width: '100%' }}
-          label="Email"
-          placeholder="Email"
-          disabled={false}
-          {...form.getInputProps('creatorEmail')}
-        />
+      {Object.keys(form.errors).find((error) => /^image/.test(error)) && (
+        <Text style={{ color: ExtraRed }} inline>
+          {Object.keys(form.errors)
+            .filter((error) => /^image/.test(error))
+            .map((key) => form.errors[key])}
+        </Text>
       )}
+      <Stack spacing={32} sx={{ maxWidth: '100%' }}>
+        <Box sx={{ maxWidth: '100%', width: rem(400) }}>
+          <Text>{'Image Requirements:'}</Text>
+          <List>
+            <List.Item>
+              <Group spacing={6}>
+                <Text>{'Mandatory: PNG or GIF format,'}</Text>
+                <Link href="/links/canva-template" target="_blank" rel="noopener noreferrer">
+                  <Text variant="link">{'GitPOAP Template'}</Text>
+                </Link>
+              </Group>
+            </List.Item>
+            <List.Item>
+              <Text>{'Recommended: measures 500x500px, size less than 200KB (Max. 4MB)'}</Text>
+            </List.Item>
+            <List.Item>
+              <Link href="/links/design-guide" target="_blank" rel="noopener noreferrer">
+                <Text variant="link">{'Design Guide'}</Text>
+              </Link>
+            </List.Item>
+          </List>
+        </Box>
+        <Input
+          required
+          style={{ width: '100%' }}
+          label="GitPOAP Name"
+          placeholder="Contributor 2022"
+          {...form.getInputProps('name')}
+        />
+        <TextArea
+          required
+          style={{ width: '100%' }}
+          label="Description"
+          placeholder="For all our valuable contributors in 2022"
+          {...form.getInputProps('description')}
+        />
+        <Box>
+          <Label mb={rem(11)} required>
+            {'Accomplishment Period'}
+          </Label>
+          <Grid>
+            <Grid.Col xs={6} span={12}>
+              <DateInput
+                maxDate={form.values.endDate}
+                placeholder="Start Date"
+                weekendDays={[]}
+                sx={{ width: '100%', minWidth: rem(220) }}
+                {...form.getInputProps('startDate')}
+              />
+            </Grid.Col>
+            <Grid.Col xs={6} span={12}>
+              <DateInput
+                minDate={form.values.startDate}
+                placeholder="End Date"
+                weekendDays={[]}
+                sx={{ width: '100%', minWidth: rem(220) }}
+                {...form.getInputProps('endDate')}
+              />
+            </Grid.Col>
+          </Grid>
+        </Box>
+        {creatorEmail ? (
+          <Input
+            required
+            style={{ width: '100%' }}
+            label="Email"
+            placeholder="Email"
+            value={creatorEmail}
+            disabled={true}
+          />
+        ) : (
+          <Input
+            required
+            style={{ width: '100%' }}
+            label="Email"
+            placeholder="Email"
+            disabled={false}
+            {...form.getInputProps('creatorEmail')}
+          />
+        )}
+        {teamName ? (
+          <Input style={{ width: '100%' }} label="Team" value={teamName} disabled={true}></Input>
+        ) : (
+          teams.teamsData && (
+            <Select
+              style={{ width: '100%' }}
+              label="Team"
+              placeholder="Pick one"
+              itemComponent={SelectItem}
+              data={[
+                ...teams.teamsData.map((team) => ({
+                  color: team.color,
+                  imageUrl: team.logoImageUrl,
+                  label: team.name,
+                  value: team.id.toString(),
+                })),
+              ]}
+              searchable
+              maxDropdownHeight={400}
+              filter={(value, item: ItemProps) =>
+                item.label.toLowerCase().includes(value.toLowerCase().trim())
+              }
+              {...form.getInputProps('teamId')}
+            />
+          )
+        )}
+      </Stack>
+      <Box my={32}>
+        <Divider
+          mb={32}
+          labelPosition="center"
+          label={<Header>{'Recipients'}</Header>}
+          variant="dashed"
+        />
+        <SelectContributors
+          contributors={form.values.contributors}
+          addContributor={(item) => form.insertListItem('contributors', item)}
+          removeContributor={(index) => form.removeListItem('contributors', index)}
+        />
+      </Box>
+      <StatusButton
+        onClick={async () => await handleSubmit(form.values)}
+        isDisabled={isDisabled}
+        status={buttonStatus}
+      >
+        {SubmitButtonText[approvalStatus]}
+      </StatusButton>
     </Stack>
-    <Box my={32}>
-      <Divider
-        mb={32}
-        labelPosition="center"
-        label={<Header>{'Recipients'}</Header>}
-        variant="dashed"
-      />
-      <SelectContributors
-        contributors={form.values.contributors}
-        addContributor={(item) => form.insertListItem('contributors', item)}
-        removeContributor={(index) => form.removeListItem('contributors', index)}
-      />
-    </Box>
-    <StatusButton
-      onClick={async () => await handleSubmit(form.values)}
-      isDisabled={isDisabled}
-      status={buttonStatus}
-    >
-      {SubmitButtonText[approvalStatus]}
-    </StatusButton>
-  </Stack>
-);
+  );
+};
